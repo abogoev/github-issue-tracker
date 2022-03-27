470bace53676638c7f014047e59d26d1d3cb7ee6
Aleksandar Bogoev <>
2022-03-27 14:59:01 +0300

    CustomTouchableOpacity

--- src/core/touchableopacity/CustomTouchableOpacity.test.tsx
+++ src/core/touchableopacity/CustomTouchableOpacity.test.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import CustomTouchableOpacity from "./CustomTouchableOpacity";
 import { create } from "react-test-renderer";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { ACTIVE_OPACITY } from "../../utils/constants";
 
 describe("CustomTouchableOpacity", () => {
   it("should render correctly", () => {
-    const component = create(<CustomTouchableOpacity />);
+    const randomText = "Hello";
+    const component = create(
+      <CustomTouchableOpacity>
+        <Text>{randomText}</Text>
+      </CustomTouchableOpacity>
+    );
     expect(
       component.root.findByType(TouchableOpacity).props.activeOpacity
     ).toBe(ACTIVE_OPACITY);
+    expect(component.root.findByType(Text).props.children).toBe(randomText);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
--- src/core/touchableopacity/CustomTouchableOpacity.tsx
+++ src/core/touchableopacity/CustomTouchableOpacity.tsx
@@ -2,7 +2,9 @@ import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import React, { FC } from "react";
 import { ACTIVE_OPACITY } from "../../utils/constants";
 
-const CustomTouchableOpacity: FC<TouchableOpacityProps> = (props) => {
+export interface CustomTouchableOpacityProps extends TouchableOpacityProps {}
+
+const CustomTouchableOpacity: FC<CustomTouchableOpacityProps> = (props) => {
   return <TouchableOpacity activeOpacity={ACTIVE_OPACITY} {...props} />;
 };
 
