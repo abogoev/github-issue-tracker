fa219a69eade3ae1c2185f15e9786df8255ba784
Aleksandar Bogoev <>
2022-04-18 10:25:20 +0300

    Pagination

---
+++ src/screens/home/pagination/Pagination.test.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "react-native";
+import Pagination from "./Pagination";
+import { create } from "react-test-renderer";
+
+describe("Pagination", () => {
+  it("should render correctly", () => {
+    const mockFunction = jest.fn();
+    const numbers = [4, 5, 6];
+
+    const component = create(
+      <Pagination
+        numbers={numbers}
+        activeIndex={0}
+        onPrev={mockFunction}
+        onNext={mockFunction}
+        onChangeActiveNumber={mockFunction}
+      />
+    );
+
+    expect(component.root.findByProps({ isActive: true }).props.text).toBe(
+      numbers[0].toString()
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("should call the callbacks correctly", () => {
+    const onPrevMock = jest.fn();
+    const onNextMock = jest.fn();
+    const onChangeActiveNumberMock = jest.fn();
+    const numbers = [4, 5, 6];
+
+    const component = create(
+      <Pagination
+        numbers={numbers}
+        activeIndex={0}
+        onPrev={onPrevMock}
+        onNext={onNextMock}
+        onChangeActiveNumber={onChangeActiveNumberMock}
+      />
+    );
+
+    expect(onPrevMock).not.toHaveBeenCalled();
+    expect(onNextMock).not.toHaveBeenCalled();
+    expect(onChangeActiveNumberMock).not.toHaveBeenCalled();
+
+    component.root.findByProps({ text: "Prev" }).props.onPress();
+
+    expect(onPrevMock).toHaveBeenCalledTimes(1);
+
+    component.root.findByProps({ text: "Next" }).props.onPress();
+
+    expect(onNextMock).toHaveBeenCalledTimes(1);
+
+    const index = 2;
+    component.root
+      .findByProps({ text: numbers[index].toString() })
+      .props.onPress();
+
+    expect(onChangeActiveNumberMock).toHaveBeenCalledWith(index);
+  });
+});
--- src/screens/home/pagination/Pagination.tsx
+++ src/screens/home/pagination/Pagination.tsx
@@ -19,9 +19,9 @@ const Pagination: VFC<Props> = ({
   onChangeActiveNumber: onChangeActiveIndex,
   ...props
 }) => {
-  return (
+  return numbers.length > 0 ? (
     <View style={[styles.container, style]} {...props}>
-      <PageItem text="Prev" onPress={onPrev} />
+      {numbers[0] !== 1 && <PageItem text="Prev" onPress={onPrev} />}
       {numbers.map((n, i) => (
         <PageItem
           key={i}
@@ -32,7 +32,7 @@ const Pagination: VFC<Props> = ({
       ))}
       <PageItem text="Next" onPress={onNext} />
     </View>
-  );
+  ) : null;
 };
 
 const styles = StyleSheet.create({
